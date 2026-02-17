4fafd00f1a80817772326881880a0c89f1b7202a
Madhan hasee <madhan796.m@gmail.com>
2026-02-17 19:49:28 +0530

    Refactored

--- emp-attendence-backend/src/routes/dashboardRoutes.js
+++ emp-attendence-backend/src/routes/dashboardRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const { protect } = require('../middleware/authMiddleware');
 const { authorize } = require('../middleware/roleMiddleware');
 
-const { getMySummary, getTeamSummary } = require('../controllers/dashboardController');
-
+// const { getMySummary, getTeamSummary } = require('../controllers/dashboardController');
+const {getMySummary, getTeamSummary} = require('../controllers/attendanceController')
 router.get('/employee', protect, getMySummary);
 
 router.get('/manager', protect, authorize('manager'), getTeamSummary);
